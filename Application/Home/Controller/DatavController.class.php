4a73bf299d4570211514440ec3d7fc441a5ba79a
Junjun Li <junjunli666@gmail.com>
2018-12-29 21:09:43 +0800

    创建 mock 数据服务端程序

---
+++ Application/Home/Controller/DatavController.class.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Home\Controller;
+
+/**
+ * Class DatavController
+ * @package Home\Controller
+ *
+ * 数据可视化项目 mock 数据 api 接口
+ */
+class DatavController extends  CommonController {
+
+    public function display() {
+
+    }
+
+    /**
+     * 第一个接口
+     */
+    public function step1() {
+
+        
+
+
+    }
+
+
+}
